94bb9177d980ac09263ea4967c38b981eb29cdc5
mahdi kashani <mahdi.kashan96@gmail.com>
2023-11-15 17:07:30 +0330

    compelete products section-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,10 @@ module.exports = {
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
     }
   },
   plugins: [
